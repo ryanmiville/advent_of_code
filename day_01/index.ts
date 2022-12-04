3913eba002acc36d03702afcd3d36dba62f47ef2
Ryan Miville <rmiville@securityscorecard.io>
2022-12-04 01:33:18 -0500

    d1p2

--- day_01/index.ts
+++ day_01/index.ts
@@ -1,21 +1,22 @@
 import { parse } from "../util.ts";
 
-class Elf {
-  calories: number[];
+type Elf = number;
 
-  constructor(calories: number[]) {
-    this.calories = calories;
-  }
-
-  total(): number {
-    return this.calories.reduce((acc, curr) => acc + curr);
-  }
+function make(calories: number[]): Elf {
+  return calories.reduce((acc, curr) => acc + curr, 0);
 }
 
-const elfs = await Deno.readTextFile("day_01/input.txt")
-  .then(parse)
-  .then((i) => i.map((c) => new Elf(c)));
+function solve(elfs: Elf[], n: number): number {
+  const topElfs: Elf[] = elfs.reduce((top, elf) => {
+    const i = top.findIndex((e) => elf >= e);
+    return (i == -1) ? top : [...top.slice(0, i), elf, ...top.slice(i, n - 1)];
+  }, new Array(n).fill(0));
 
-const answer = Math.max(...elfs.map((e) => e.total()));
+  return topElfs.reduce((acc, curr) => acc + curr, 0);
+}
+
+const input = await Deno.readTextFile("day_01/input.txt");
+const elfs = parse(input).map(make);
+const answer = solve(elfs, 3);
 
 console.log(answer);
