0066d8857997c27b0b8ba2208ac86f0d8d7715b0
Ryan Miville <rmiville@securityscorecard.io>
2022-12-05 22:26:44 -0500

    d3p2

--- day_03/index.ts
+++ day_03/index.ts
@@ -1,16 +1,21 @@
-const input = await Deno.readTextFile("day_03/input.txt");
+import { chunk } from "../util.ts";
 
 function itemValue(str: string): number {
   const ascii = str.charCodeAt(0);
   return ascii < 97 ? ascii - 38 : ascii - 96;
 }
+const badge = (elfs: string[]): number => {
+  const [e1, e2, e3] = elfs;
+  const r1 = new Set<string>([...e1]);
+  const r2 = [...e2];
+  const r3 = [...e3];
 
-const lines = input.split("\n");
-const answer = lines.reduce((acc, l) => {
-  const mid = l.length / 2;
-  const c1 = new Set<string>([...l.slice(0, mid)]);
-  const c2 = [...l.slice(mid)];
-  return acc + itemValue(c2.find((e) => c1.has(e))!);
-}, 0);
+  const inBoth = new Set<string>(r2.filter((i) => r1.has(i)));
+  return itemValue(r3.find((i) => inBoth.has(i))!);
+};
+
+const input = await Deno.readTextFile("day_03/input.txt");
+const groups = chunk(input.split("\n"), 3);
+const answer = groups.reduce((acc, group) => acc + badge(group), 0);
 
 console.log(answer);
--- util.ts
+++ util.ts
@@ -1,3 +1,8 @@
 export function parse(input: string): number[][] {
   return input.split("\n\n").map((str) => str.split("\n").map((s) => +s));
 }
+
+export const chunk = <T>(arr: T[], size: number): T[][] =>
+  [...Array(Math.ceil(arr.length / size))].map((_, i) =>
+    arr.slice(size * i, size + size * i)
+  );
