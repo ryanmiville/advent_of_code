b138366d7455b71b5b6751c899eebe66f60dbe49
Ryan Miville <ryan.miville@terminus.com>
2024-12-01 10:58:31 -0500

    2024

--- README.md
+++
@@ -1,6 +0,0 @@
-# 2022 - Deno TypeScript with Rust FFI
-
-```sh
-deno_bindgen
-deno run -A --unstable day1.ts
-```
--- util.ts
+++
@@ -1,8 +0,0 @@
-export function parse(input: string): number[][] {
-  return input.split("\n\n").map((str) => str.split("\n").map((s) => +s));
-}
-
-export const chunk = <T>(arr: T[], size: number): T[][] =>
-  [...Array(Math.ceil(arr.length / size))].map((_, i) =>
-    arr.slice(size * i, size + size * i)
-  );
